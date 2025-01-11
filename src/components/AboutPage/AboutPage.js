2665fc7759af1ae243476cef84a6abb681f5e170
isaacotim101 <135863652+isaacotim101@users.noreply.github.com>
2025-01-11 13:04:36 +0300

    history

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -51,7 +51,13 @@ const AboutPage = () => {
               </h3>
 
               <p style={{ textAlign: "justify" }}>
-              Epilusis was founded in 2014 as a revival movement aimed at equipping and empowering high school students, primarily with students from Kyambogo College School and City High. These gatherings, known as Epilusis Classes, were not just meetings but intentional spaces where students—many of whom had experienced the baptism of the Holy Spirit—could be deeply grounded in the Word of God.              </p>
+              Epilusis was founded in 2014 as a revival movement aimed at equipping and empowering high school students, primarily with students from Kyambogo College School and City High. These gatherings, known as Epilusis Classes, were not just meetings but intentional spaces where students—many of whom had experienced the baptism of the Holy Spirit—could be deeply grounded in the Word of God.             
+               </p>
+              <p style={{ textAlign: "justify" }}>
+              Much like the early church, where the Spirit fell upon those gathered in the upper room (Acts 2:1-4), these students experienced a fresh move of God. But just as the believers in the early church were led to Antioch, where they received much teaching (Acts 11:26 KJV), Epilusis Classes became a place of spiritual formation, ensuring that young believers were not only filled with the Spirit but also rooted in biblical truth.               </p>
+              <p style={{ textAlign: "justify" }}>
+              Over the years, Epilusis has evolved in response to God’s leading and the divine demands of the season. What began as a discipleship class soon grew into a fellowship, and today, Epilusis stands as a mission-oriented ministry with a vision to plant churches and establish a home church—a place where believers are equipped, empowered, and sent out to transform nations.               </p>
+
             </div>
           </Col>
         </Row>
